1ddf4fa64e8971b30d309fd97bf01b6ab8d00ebd
jaseem1033 <jaseem1033@gmail.com>
2025-04-26 10:28:55 +0530

    Time: 2 ms (28.44%) | Memory: 57.1 MB (85.57%) - LeetSync

--- 189-rotate-array/rotate-array.java
+++ 189-rotate-array/rotate-array.java
@@ -9,31 +9,5 @@ class Solution {
         }
         for(int i=0;i<n;i++)
         nums[i]=arr[i];
-
-
-
-
-
-
-
-
-        /*int n=nums.length;
-        int a;
-        if(nums==1)
-        a++;
-        else{
-        List<Integer> arr=new ArrayList<>();
-        for(int i=n-k;i<n;i++)
-        arr.add(nums[i]);
-
-        for(int i=0;i<n-k;i++)
-        arr.add(nums[i]);
-
-        for(int i=0;i<n;i++)
-        nums[i]=arr.get(i);
-        }*/
-        
-
-
     }
 }
