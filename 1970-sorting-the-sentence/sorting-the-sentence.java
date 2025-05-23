b376055eb1cc1676b098386b9efd5249026e361b
jaseem1033 <jaseem1033@gmail.com>
2025-05-23 12:44:30 +0530

    Time: 1 ms (40.00%) | Memory: 41.4 MB (88.04%) - LeetSync

---
+++ 1970-sorting-the-sentence/sorting-the-sentence.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String sortSentence(String s) {
+        String arr[] = new String[10];
+        String splitArr[] = s.split(" ");
+        for(String str : splitArr)
+        {
+            int idx = Integer.valueOf(str.substring(str.length()-1,str.length()));
+            arr[idx] = str.substring(0,str.length()-1);
+        }
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < arr.length; i++)
+        {
+            if(arr[i] == null) continue;
+            sb.append(arr[i]+" ");
+        }
+        sb.deleteCharAt(sb.length()-1);
+        return sb.toString();
+    }
+}
