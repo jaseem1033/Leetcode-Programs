20b7078f4f154febe3bdba6ce4bc269a4b2655c6
jaseem1033 <jaseem1033@gmail.com>
2025-05-30 17:17:56 +0530

    Time: 55 ms (24.25%) | Memory: 46.1 MB (34.02%) - LeetSync

--- 1031-add-to-array-form-of-integer/add-to-array-form-of-integer.java
+++ 1031-add-to-array-form-of-integer/add-to-array-form-of-integer.java
@@ -5,16 +5,19 @@ class Solution {
         int n = num.length-1;
         int carry = 0;
 
-        while(k > 0 || n >= 0) {
+        while(k > 0 || n >= 0 || carry == 1) {
+            
             int val;
+
             if(n < 0) val = k % 10 + carry;
             else val = (k%10) + num[n] + carry;
+
             carry = val/10;
             list.add(0, val % 10);
+
             k /= 10;
             n--;
         }
-        if(carry == 1) list.add(0,1);
         return list;
     }
 }
