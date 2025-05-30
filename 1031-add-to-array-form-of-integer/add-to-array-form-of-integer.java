4efd74884a5892a2f21a3c84fc51a161f0f2e9f9
jaseem1033 <jaseem1033@gmail.com>
2025-05-30 17:15:45 +0530

    Time: 55 ms (24.25%) | Memory: 46 MB (46.02%) - LeetSync

--- 1031-add-to-array-form-of-integer/add-to-array-form-of-integer.java
+++ 1031-add-to-array-form-of-integer/add-to-array-form-of-integer.java
@@ -9,14 +9,8 @@ class Solution {
             int val;
             if(n < 0) val = k % 10 + carry;
             else val = (k%10) + num[n] + carry;
-            if(val > 9) {
-                carry = 1;
-                list.add(0, val % 10);
-            }
-            else {
-                list.add(0,val);
-                carry = 0;
-            }
+            carry = val/10;
+            list.add(0, val % 10);
             k /= 10;
             n--;
         }
