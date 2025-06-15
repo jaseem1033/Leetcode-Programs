ec24ebff4ed86f096d147b32f5da16dcf8f52816
jaseem1033 <jaseem1033@gmail.com>
2025-06-15 16:20:46 +0530

    Time: 3 ms (45.42%) | Memory: 45.6 MB (15.04%) - LeetSync

--- 47-permutations-ii/permutations-ii.java
+++ 47-permutations-ii/permutations-ii.java
@@ -1,32 +1,30 @@
 class Solution {
     List<List<Integer>> result;
-    HashSet<List<Integer>> set;
 
 public List<List<Integer>> permuteUnique(int[] nums) {
         result = new ArrayList<>();
-        set = new HashSet<>();
         Integer[] arr = new Integer[nums.length];
         for(int i = 0; i < nums.length; i++)
         arr[i] = nums[i];
+        Arrays.sort(arr);
         backTrack(0,arr);
         return result;
     }
-
     public void swap(int x, int y, Integer[] nums) {
         int temp = nums[x];
         nums[x] = nums[y];
         nums[y] = temp;
     }
-
     public void backTrack(int index,Integer[] nums) {
         if(index == nums.length) {
-            if(!set.contains(new ArrayList<>(Arrays.asList(nums)))){
             result.add(new ArrayList<>(Arrays.asList(nums)));
-            set.add(new ArrayList<>(Arrays.asList(nums)));}
             return;
         }
 
+        HashSet<Integer> set = new HashSet<>();
         for(int i = index; i < nums.length; i++) {
+            if(set.contains(nums[i])) continue;
+            set.add(nums[i]);
             swap(index,i,nums);
             backTrack(index + 1, nums);
             swap(index,i,nums);
