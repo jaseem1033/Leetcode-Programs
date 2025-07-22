3d880d2c28105931283409bdbddf93340c8fbe53
JASEEM ATHRAPPIL <jaseem1033@gmail.com>
2025-07-22 21:59:32 +0530

    Time: 3 ms (47.09%) | Memory: 45.5 MB (85.46%) - LeetSync

--- 800-letter-case-permutation/letter-case-permutation.java
+++ 800-letter-case-permutation/letter-case-permutation.java
@@ -1,21 +1,25 @@
 class Solution {
     String s;
     List<String> result = new ArrayList<>();
+
     public List<String> letterCasePermutation(String s) {
         int n = s.length();
-        this.s = new String(s);
+        this.s = s;
         backTrack(0, new StringBuilder(), n);
         return result;
     }
+
     public void backTrack(int index, StringBuilder str, int n) {
         if(str.length() == n) {
             result.add(str.toString());
             return;
         }
+
         if(Character.isLetter(s.charAt(index))) {
             backTrack(index + 1, new StringBuilder(str).append(Character.toUpperCase(s.charAt(index))), n);
             backTrack(index + 1, new StringBuilder(str).append(Character.toLowerCase(s.charAt(index))), n);
         }
+
         else {
             backTrack(index + 1, new StringBuilder(str).append(s.charAt(index)), n);
         }
