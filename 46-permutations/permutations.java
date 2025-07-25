5b0d5ca8ee29176783d96ee9a0fa5287b6a39a43
JASEEM ATHRAPPIL <jaseem1033@gmail.com>
2025-07-26 00:03:04 +0530

    Time: 1 ms (92.65%) | Memory: 44.5 MB (63.51%) - LeetSync

--- 46-permutations/permutations.java
+++ 46-permutations/permutations.java
@@ -1,18 +1,17 @@
 class Solution {
     List<List<Integer>> result;
+
     public List<List<Integer>> permute(int[] nums) {
         result = new ArrayList<>();
         Integer[] arr = new Integer[nums.length];
+
         for(int i = 0; i < nums.length; i++)
         arr[i] = nums[i];
+
         backTrack(0,arr);
         return result;
     }
-    public void swap(int x, int y, Integer[] nums) {
-        int temp = nums[x];
-        nums[x] = nums[y];
-        nums[y] = temp;
-    }
+
     public void backTrack(int index,Integer[] nums) {
         if(index == nums.length) {
             result.add(new ArrayList<>(Arrays.asList(nums)));
@@ -24,5 +23,11 @@ class Solution {
             backTrack(index + 1, nums);
             swap(index,i,nums);
         }
+    }
+    
+    public void swap(int x, int y, Integer[] nums) {
+        int temp = nums[x];
+        nums[x] = nums[y];
+        nums[y] = temp;
     }
 }
