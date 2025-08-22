8ff13fe55d8bb4581737b83384dabe00c25252a4
JASEEM ATHRAPPIL <jaseem1033@gmail.com>
2025-08-22 22:00:30 +0530

    Time: 5 ms (47.14%) | Memory: 56 MB (82.58%) - LeetSync

--- 643-maximum-average-subarray-i/maximum-average-subarray-i.java
+++ 643-maximum-average-subarray-i/maximum-average-subarray-i.java
@@ -3,16 +3,13 @@ class Solution {
         int l = 0, r;
         double sum = 0;
 
-        for(int i = 0; i < k; i++) sum += nums[i]; 
-        sum /= k;
+        for(int i = 0; i < k; i++) sum += nums[i];
         double max = sum;
         for(r = k; r < nums.length; r++) {
-            max *= k;
             max += nums[r];
             max -= nums[l++];
-            max /= k;
             sum = Math.max(sum, max);
         }
-        return sum;
+        return sum/k;
     }
 }
