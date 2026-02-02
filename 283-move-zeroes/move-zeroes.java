a47335022012c351027ad6f5568cb0f13a75d508
JASEEM ATHRAPPIL <jaseem1033@gmail.com>
2026-02-02 22:41:52 +0530

    Time: 3 ms (19.66%) | Memory: 47.8 MB (70.83%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.java
@@ -0,0 +1,20 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int j = 0;
+        for(int i = 1; i < nums.length; i++) {
+            if(nums[j] == 0) {
+                if(nums[i] != 0) {
+                    swap(nums, j, i);
+                    j++;
+                }
+            }
+            else j++;
+        }
+    }
+
+    public void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}
