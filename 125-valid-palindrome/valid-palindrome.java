82b8455441a08e8c26832a15863b0bc19101531e
JASEEM ATHRAPPIL <jaseem1033@gmail.com>
2026-02-05 13:00:55 +0530

    Time: 4 ms (51.44%) | Memory: 45.6 MB (39.98%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -1,19 +1,14 @@
 class Solution {
     public boolean isPalindrome(String s) {
+
         s = s.toLowerCase();
         StringBuilder str = new StringBuilder();
         for(char c : s.toCharArray()) {
-            if(Character.isLetterOrDigit(c)) {
-                str.append(c);
-            }
+            if(Character.isLetterOrDigit(c)) str.append(c);
         }
-        int left = 0; 
-        int right = str.length()-1;
-        while(left < right) {
-            if(str.charAt(left) != str.charAt(right)) return false;
-            left++;
-            right--;
-        }
-        return true;
+
+
+        return (str.toString().equals(str.reverse().toString()));
+
     }
 }
