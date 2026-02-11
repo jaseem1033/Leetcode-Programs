9d657e099a3895165752e5ecf4e906bb6ef98814
JASEEM ATHRAPPIL <jaseem1033@gmail.com>
2026-02-11 23:58:51 +0530

    Time: 7 ms (51.91%) | Memory: 44.2 MB (26.76%) - LeetSync

--- 1297-maximum-number-of-balloons/maximum-number-of-balloons.java
+++ 1297-maximum-number-of-balloons/maximum-number-of-balloons.java
@@ -1,23 +1,27 @@
 class Solution {
     public int maxNumberOfBalloons(String text) {
-        HashMap<Character,Integer>hm=new HashMap<>();
-        hm.put('b',0);
-        hm.put('a',0);
-        hm.put('l',0);
-        hm.put('o',0);
-        hm.put('n',0);
-        for(char c:text.toCharArray())
-        {
-            if(hm.containsKey(c))
+        HashMap<Character, Integer> map = new HashMap<>();
+        map.put('b', 0);
+        map.put('a', 0);
+        map.put('l', 0);
+        map.put('o', 0);
+        map.put('n', 0);
+
+        for(char c : text.toCharArray()) {
+            if(map.containsKey(c))
             {
-                hm.put(c,hm.get(c)+1);
+                map.put(c, map.get(c) + 1);
             }
         }
-        hm.put('l',hm.get('l')/2);
-        hm.put('o',hm.get('o')/2);
-        int count=10000;
-        for(int i:hm.values())
-        count=Math.min(count,i);
-        return count;
+
+        int max = Integer.MAX_VALUE;
+        map.put('o', map.get('o')/2);
+        map.put('l', map.get('l')/2);
+
+        for(int i : map.values()) {
+            max = Math.min(i, max);
+        }
+
+        return max;
     }
 }
