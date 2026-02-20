5872c112c7625fdc78ddaa9cbc3997f3b68c4d6c
JASEEM ATHRAPPIL <jaseem1033@gmail.com>
2026-02-20 11:02:17 +0530

    Time: 1 ms (96.12%) | Memory: 43.2 MB (39.95%) - LeetSync

--- 873-guess-the-word/guess-the-word.java
+++ 873-guess-the-word/guess-the-word.java
@@ -9,7 +9,7 @@ class Solution {
     public void findSecretWord(String[] words, Master master) {
         List<String> list = new ArrayList<>(Arrays.asList(words));
 
-        while(!list.isEmpty()) {
+        while(true) {
 
                 String word = list.get(new Random().nextInt(list.size()));
 
