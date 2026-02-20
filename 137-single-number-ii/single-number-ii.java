4b3d10fdc828a60eb70d5b2847fd64e9ab955873
JASEEM ATHRAPPIL <jaseem1033@gmail.com>
2026-02-20 22:44:30 +0530

    Time: 4 ms (64.81%) | Memory: 45.2 MB (76.27%) - LeetSync

--- 137-single-number-ii/single-number-ii.java
+++ 137-single-number-ii/single-number-ii.java
@@ -4,12 +4,10 @@ class Solution {
         int res = 0;
 
         for (int i = 0; i <= 31; i++) {
-            int one = 0, zero = 0;
+            int one = 0;
             for (int j = 0; j < nums.length; j++) {
                 if ((nums[j] & 1) == 1)
                     one++;
-                else
-                    zero++;
                 nums[j] >>= 1;
             }
             int bit = one % 3 == 0 ? 0 : 1;
