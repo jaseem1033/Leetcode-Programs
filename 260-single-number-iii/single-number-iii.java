298a52fcdab19ef8807e0630eef471707eb79983
JASEEM ATHRAPPIL <jaseem1033@gmail.com>
2026-02-22 22:44:11 +0530

    Time: 1 ms (99.74%) | Memory: 48 MB (59.75%) - LeetSync

---
+++ 260-single-number-iii/single-number-iii.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int[] singleNumber(int[] nums) {
+        int xorBit = 0;
+        for(int x : nums) {
+            xorBit ^= x;
+        }
+
+        int setBit = 1;
+        while(xorBit != 0) {
+            if((xorBit & 1) == 1) {
+                break;
+            } 
+            setBit = setBit << 1;
+            xorBit = xorBit >> 1;
+        }
+        int res1 = 0, res2 = 0;
+
+        for(int i : nums) {
+            if((i & setBit) == 0)
+            res1 ^= i;
+            else 
+            res2 ^= i;
+        }
+        return new int[]{res1, res2};
+    }
+}
